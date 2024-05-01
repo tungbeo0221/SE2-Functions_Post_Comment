2f66b750ab4dd867f863e2073b0739dcb5c6b6f1
Tung Nguyen X <118703750+tungbeo0221@users.noreply.github.com>
2024-05-01 21:12:40 +0700

    Delete repository directory

--- repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package SE2.SocialMediaProject.repository;
-
-import SE2.SocialMediaProject.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package SE2.SocialMediaProject.repository;
-
-import
-import SE2.SocialMediaProject.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
-}
