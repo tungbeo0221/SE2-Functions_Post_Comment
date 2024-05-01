3254fe7ceec2ff22ea794ae85f72a4174ddfe2b5
Tung Nguyen X <118703750+tungbeo0221@users.noreply.github.com>
2024-05-01 21:11:35 +0700

    Add files via upload

---
+++ repository/PostRepository.java
@@ -0,0 +1,9 @@
+package SE2.SocialMediaProject.repository;
+
+import
+import SE2.SocialMediaProject.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+}
