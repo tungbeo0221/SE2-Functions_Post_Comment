c2bbf91f8bcdf9d2de3b70889bdb61b9a415eeba
Tung Nguyen X <118703750+tungbeo0221@users.noreply.github.com>
2024-05-01 19:14:12 +0700

    Add files via upload

---
+++ SocialMediaProject/src/test/java/SE2/SocialMediaProject/SocialMediaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SE2.SocialMediaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
